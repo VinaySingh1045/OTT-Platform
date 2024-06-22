bc6f00a4d8c3e01966d9241cb298678a98bd49e4
VinaySingh1045 <vy0891547@gmail.com>
2024-06-22 11:55:24 +0530

    fixing errorss

--- app/contact/page.js
+++ app/contact/page.js
@@ -15,7 +15,7 @@ const Contact = () => {
       </div>
       <div>
         <h1 className='flex flex-col sm:flex-row sm:justify-center sm:items-center text-4xl font-bold my-8 text-center'>
-          We&apos;d love to hear&nbsp;<span className='text-blue-500'>from you</span>
+          {"We'd love to hear"}&nbsp;<span className='text-blue-500'>from you</span>
         </h1>
 
         <ContactForm />
@@ -23,7 +23,6 @@ const Contact = () => {
       <div className='mt-5 w-full mb-10'>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29768.55573706565!2d72.81092081217187!3d21.14963357923498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be051d9bc6b9563%3A0xf0b8f0ee0459d502!2zQXZpcmJoYXYgU29jaWV0eS0yIC0g4KqF4Kq14Kq_4Kqw4KuN4Kqt4Kq-4Kq1IOCquOCri-CquOCqvuCqr-Cqn-CrgCAtIDI!5e0!3m2!1sen!2sin!4v1718332348688!5m2!1sen!2sin" width="100%" height={450} style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
-      {/* h */}
     </>
   )
 }
