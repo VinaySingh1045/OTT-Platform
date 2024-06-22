e27fc2bf1c0831b609593501444158f9e24f4997
VinaySingh1045 <vy0891547@gmail.com>
2024-06-22 11:30:17 +0530

    Someerror fixed

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <>
       <div>
-        <Hero title={"LET'S WATCH MOVIE TOGETHER"} imageurl={"/home.svg"} para={"Let’s watch a movie together! We'll relax with popcorn and blankets, enjoying every moment. There's nothing like sharing a great film with a good friend. Join me for a night of fun and memories."} />
+        <Hero title={"LET'S WATCH MOVIE TOGETHER"} imageurl={"/home.svg"} para={"Let's watch a movie together! We'll relax with popcorn and blankets, enjoying every moment. There's nothing like sharing a great film with a good friend. Join me for a night of fun and memories."} />
       </div>
     </>
   )
